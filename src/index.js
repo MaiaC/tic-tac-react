219539ab279445baec79d2a8ec2d3725f604f1a6
Maia Coghlan <maiaccog@gmail.com>
2019-05-25 16:56:14 +0800

    remove for loop

--- src/index.js
+++ src/index.js
@@ -145,12 +145,6 @@ function calculateWinner(squares) {
       result = squares[a];
     }
   })
-  
-  // for (let i = 0; i < lines.length; i++) {
-  //   const [a, b, c] = lines[i];
-  //   if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-  //     return squares[a];
-  //   }
-  // }
+
   return result;
 }
